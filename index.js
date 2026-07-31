11df7ae9f35034bf3b2e72b53ad920667dc2baa6
Varis Karklins <vkarklins@outlook.com>
2026-07-31 09:34:43 -0700

    updated PORT settings for web service deployment

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note);
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
